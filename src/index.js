8964edb5411040a302b81f47f1c7e52d1d4bda5b
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-02-19 20:20:19 +0200

    ver 1.0

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './js/refs';
 import { Notify } from 'notiflix';
 import fetchData from './js/fetchData';
-import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import debounce from 'lodash.debounce';
 
 let page = 1;
 
@@ -20,44 +20,67 @@ refs.form.addEventListener('submit', e => {
     fetchData(q, page)
       .then(markup => (refs.gallery.innerHTML = markup))
       .then(data => {
-        if (data) refs.loadMoreBtn.classList.remove('hidden');
+        // if (data) refs.loadMoreBtn.classList.remove('hidden'); //! use it if you want use button
         gallery.refresh();
       })
       .catch(error => console.error(error));
   }
 });
 
-refs.loadMoreBtn.addEventListener('click', e => {
-  //TODO MAKE REALLY CONSTANT IN GLOBAL CODE, DON'T USE 2 TIMES SAME CODE
-  const q = refs.input.value;
+//! case for button scroll
+// refs.loadMoreBtn.addEventListener('click', e => {
+//   const q = refs.input.value;
 
-  if (!q)
-    Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
-  else {
-    refs.loadMoreBtn.classList.add('hidden');
-    page += 1;
-    fetchData(q, page)
-      .then(markup => refs.gallery.insertAdjacentHTML('beforeend', markup))
-      .then(data => {
-        refs.loadMoreBtn.classList.remove('hidden');
-        gallery.refresh();
+//   if (!q)
+//     Notify.failure(
+//       'Sorry, there are no images matching your search query. Please try again.'
+//     );
+//   else {
+//     refs.loadMoreBtn.classList.add('hidden');
+//     page += 1;
+//     fetchData(q, page)
+//       .then(markup => refs.gallery.insertAdjacentHTML('beforeend', markup))
+//       .then(data => {
+//         refs.loadMoreBtn.classList.remove('hidden');
+//         gallery.refresh();
 
-        const { height: cardHeight } = document
-          .querySelector('.gallery')
-          .firstElementChild.getBoundingClientRect();
+//         const { height: cardHeight } = document
+//           .querySelector('.gallery')
+//           .firstElementChild.getBoundingClientRect();
 
-        window.scrollBy({
-          top: cardHeight * 2,
-          behavior: 'smooth',
-        });
-      })
-      .catch(error => console.error(error));
-  }
-});
+//         window.scrollBy({
+//           top: cardHeight * 2,
+//           behavior: 'smooth',
+//         });
+//       })
+//       .catch(error => {
+//         console.error(error);
+//         refs.loadMoreBtn.classList.add('hidden');
+//       });
+//   }
+// });
 
 let gallery = new SimpleLightbox(`.gallery a`, {
   captionsData: 'alt',
   captionDelay: 250,
 });
+
+window.addEventListener(
+  'scroll',
+  debounce(e => {
+    const q = refs.input.value;
+    const documentRect = document.documentElement.getBoundingClientRect();
+    if (documentRect.bottom < document.documentElement.clientHeight + 1000) {
+      page += 1;
+      fetchData(q, page)
+        .then(markup => refs.gallery.insertAdjacentHTML('beforeend', markup))
+        .then(data => {
+          gallery.refresh();
+        })
+        .catch(error => {
+          console.error(error);
+          refs.loadMoreBtn.classList.add('hidden');
+        });
+    }
+  }, 300)
+);
--- src/js/fetchData.js
+++ src/js/fetchData.js
@@ -2,26 +2,39 @@ import createCard from './createCard';
 import { Notify } from 'notiflix';
 import axios from 'axios';
 
-//TODO Need to add axios
 axios.defaults.baseURL = 'https://pixabay.com';
 
-export default function fetchData(q, page) {
-  return fetch(
-    `https://pixabay.com/api/?q=${q}&key=33730392-00e87f60b0c2dabc7d687ed2e&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
-  )
-    .then(response => response.json())
-    .then(data => {
-      if (!data.total) {
-        Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-        refs.loadMoreBtn.classList.add('hidden');
-      }
-      if (page === 1 && data.total)
-        Notify.success(`Hooray! We found ${data.totalHits} images.`);
+export default async function FetchData(q, page) {
+  const { data } = await axios.get(`/api/`, {
+    params: {
+      key: `33730392-00e87f60b0c2dabc7d687ed2e`,
+      q: `${q}`,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      per_page: 40,
+      page: `${page}`,
+      safesearch: true,
+    },
+  });
 
-      return createMarkup(data.hits);
-    });
+  if (!data.total) {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    refs.loadMoreBtn.classList.add('hidden');
+  }
+  if (page === 1 && data.total)
+    Notify.success(`Hooray! We found ${data.totalHits} images.`);
+
+  console.log(data);
+  if (data.hits == 0 && data.total) {
+    Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+    throw new Error('No more results');
+  }
+
+  return createMarkup(data.hits);
 }
 
 function createMarkup(data) {
