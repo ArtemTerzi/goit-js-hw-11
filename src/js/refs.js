6ff4a6743bf2a222e8d87a32fb53d1d98c25c2d1
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-02-19 20:49:53 +0200

    ver 1.02

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,7 +1,9 @@
-export default refs = {
+const refs = {
   form: document.querySelector('.search-form'),
   input: document.querySelector('.form-thumb > input'),
   searchBtn: document.querySelector('.search-btn'),
   gallery: document.querySelector('.gallery'),
   loadMoreBtn: document.querySelector('.load-more'),
 };
+
+export default refs;
